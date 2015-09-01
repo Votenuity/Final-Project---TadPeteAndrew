40b577c4568cdf029c6ae2d6d6edd50516b6294a
andrewariley87 <andrewariley87@gmail.com>
2015-09-01 10:02:42 -0400

    wizard initial steps in setting up the wizard

---
+++ app/helpers/sign_up_steps_helper.rb
@@ -0,0 +1,2 @@
+module SignUpStepsHelper
+end
---
+++ test/controllers/sign_up_steps_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SignUpStepsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
