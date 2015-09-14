e9d666c58244ab7e11f8b05f2166c3b6f58e19fb
esttres <esttres@hotmail.com>
2015-09-14 10:29:49 -0400

    added rake command ability with/without year added

--- lib/get_legislators.rb
+++ lib/get_legislators.rb
@@ -45,7 +45,7 @@ class LegislatorGrabber
   # Creates URL for database store
   def leg_pic_actual_url(cur_session, link)
 
-    "http://iga.in.gov/legislative/#{cur_session}/portraits/legislator_#{remove_front_of_url(link)}"
+    "http://iga.in.gov/legislative/#{cur_session.to_s}/portraits/legislator_#{remove_front_of_url(link)}"
 
   end
 
@@ -53,7 +53,7 @@ class LegislatorGrabber
   def leg_grab(cur_session)
 
     # Grabs JSON from API, using only endpoint, parses in to array of hashes
-    leg_hash = parsed("https://api.iga.in.gov/#{cur_session}/legislators?per_page=160")
+    leg_hash = parsed("https://api.iga.in.gov/#{cur_session.to_s}/legislators?per_page=160")
 
 
     # Assigns each house member to a spot in the database
@@ -76,7 +76,7 @@ class LegislatorGrabber
 
       # Creates Legislators from API stream
 
-      Legislator.create(session: cur_session,
+      Legislator.create(session: cur_session.to_s,
                         position_title: house[:position_title],
                         firstName: house[:firstName],
                         lastName: house[:lastName],
@@ -99,7 +99,7 @@ class LegislatorGrabber
 
   def cill_grab(cur_session)
 
-    all_bills_hash = parsed("https://api.iga.in.gov/#{cur_session}/bills?per_page=1560")
+    all_bills_hash = parsed("https://api.iga.in.gov/#{cur_session.to_s}/bills?per_page=1560")
 
     all_bills_hash[:items].each do |item|
 
